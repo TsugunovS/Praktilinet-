3a8fe250f2430d3c3076d30dafd1106fb531a8e3
opilane <opilane@HP-CZC6297B8Z>
2022-12-14 15:40:11 +0200

    Add project files.

---
+++ Praktilinetöö/Praktilinetöö.py
@@ -0,0 +1,25 @@
+from math import *
+import numbers 
+from random import * 
+#esimene ülesanne
+print("sisestage suvaline number")
+n = int(input("sisestage number: "))
+try:
+    if n>0:
+        print("positiivne arv")
+        if a%2==0
+            print(f"{a} on paaris")
+        else:
+            print(f"{a} on paaritu")
+    else:
+        print("negatiivne arv")
+except:
+    print("Vale andmetüüp")
+print()
+
+
+#2
+print("")
+num1=int(input("esimene number"))
+num2=int(input("teine ​​number"))
+num3=int(input("kolmas number"))
